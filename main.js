41068f55e7032df0d33df4ccc6e448b0522af3b5
Borja Alventosa <borjaalventosa@gmail.com>
2023-01-26 18:50:08 +0100

    telegram asfasdfa

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
-// main.js que utilizando Nodejs (la librería node-telegram-bot-api) enviará un mensaje a 722271252
+const TOKEN = require()
 
-// Importamos la librería node-telegram-bot-api
 const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(secrets.TOKEN, { polling: true });
 
-//Cogemos los secrets Telegram_Token y el Telegram_ChatID de las variables de entorno definidas en github
+const CHAT_ID = secrets.CHAT_ID;
 
-// Creamos un bot que utiliza 'polling' para obtener actualizaciones
-const bot = new TelegramBot(TOKEN, { polling: true });
-//Le ponemos nombre y lo ponemos en escucha de mensajes
-
-console.log('Bot server started in the port 3000');  
-
-bot.getMe().then((me) => {
-   bot.sendMessage(CHAT_ID, 'Hola, soy ' + me.first_name + ' y estoy activo');
-});
+console.log('Bot server started in the port 3000');
 
 bot.sendMessage(CHAT_ID, 'Hola, soy un bot');
-bot.sendMessage(CHAT_ID, 'Y estamos activos papi! jajajaja');
-
-bot.sendMessage(CHAT_ID, 'Workflow ejecutado correctamente tras el último commit. Saludos '. NOMBRE);
+bot.sendMessage(CHAT_ID, 'Workflow ejecutado correctamente tras el último commit. Saludos '.NOMBRE);
 
 console.log('Mensaje enviado');
