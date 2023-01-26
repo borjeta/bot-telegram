9df8769ef21865a44d9f19a9dcedd330f96a4bb5
Borja Alventosa <borjaalventosa@gmail.com>
2023-01-26 17:44:39 +0100

    telegram asfasdfa

--- main.js
+++ main.js
@@ -5,15 +5,10 @@ const TelegramBot = require('node-telegram-bot-api');
 
 //Cogemos los secrets Telegram_Token y el Telegram_ChatID de las variables de entorno definidas en github
 
-const TOKEN = process.env.secrets.TELEGRAM_TOKEN;
-const CHAT_ID = process.env.secrets.TELEGRAM_CHAT_ID;
-
 // Creamos un bot que utiliza 'polling' para obtener actualizaciones
 const bot = new TelegramBot(TOKEN, { polling: true });
 //Le ponemos nombre y lo ponemos en escucha de mensajes
 
- 
- 
 console.log('Bot server started in the port 3000');  
 
 bot.getMe().then((me) => {
