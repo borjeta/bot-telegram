83ea2f5fad22cc6103f9d443892e10b2830c7366
Borja Alventosa <borjaalventosa@gmail.com>
2023-01-27 16:35:13 +0100

    prueba telegramsdfa1231

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const core = require('@actions/core');
 
 //Cogemos los secrets Telegram_Token y el Telegram_ChatID de las variables de entorno definidas en github
 
-const TOKEN = core.getVar('TOKEN');
+const TOKEN = core.getInput('TOKEN');
 const CHAT_ID = core.getInput('CHAT_ID');
 const NOMBRE = core.getInput('NOMBRE');
 
